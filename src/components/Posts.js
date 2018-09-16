c54e25e3cf00aefa65cce4bbe0d047d0bcb9cc3e
Timothy Hoang <timh1203@gmail.com>
2018-09-15 22:37:17 -0400

    Add axios and api call

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Posts extends Component {
   componentWillMount() {
-
+    axios
+      .get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => console.log(res.data))
   }
   render() {
     return (
